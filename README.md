8d0c56dcb2d84ad471e7fc584be3223c21d39d2f
Johannes Reiter <jgreit@gmail.com>
2021-01-04 14:06:53 -0800

    rll v0.1.3 2021-01-04: Added functions to convert different measures of tumor sizes.

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ To install the latest release of rll, run ```pip install rll```
 ### <a name="releases"> Releases
 * rll v0.1.0 2020-12-11: Initial release of package.
 * rll v0.1.2 2020-12-11: Added flexibility with legend display.
-* rll v0.1.3 2020-12-11: Added functions to convert different measures of tumor sizes.
+* rll v0.1.3 2021-01-04: Added functions to convert different measures of tumor sizes.
 
 ### License
 These methods are licensed under the GNU General Public License, Version 3. The methods are free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License. There is no warranty for this free software.
---
+++ tests/test_tumorsize.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""Unit tests for converting different measures of tumor size"""
+
+__author__ = 'Johannes REITER'
+__date__ = 'January 4, 2021'
+
+import pytest
+
+from rll.tumorsize import *
+
+
+@pytest.mark.parametrize('d_cm, volume',
+                         [(0.0, 0.0), (0.1, 5.236e-4), (1, 0.5236)])
+def test_sphere_volume(d_cm, volume):
+
+    assert sphere_volume(d_cm) == pytest.approx(volume, rel=1e-3)
+
+
+# @pytest.mark.skip(reason='not yet implemented')
+@pytest.mark.parametrize('d_cm, cells_per_cm3, n_cells',
+                         [(0.0, 1e9, 0), (1, 1e9, 5.23598e8), (1, 0, 0)])
+def test_diameter_cells(d_cm, cells_per_cm3, n_cells):
+
+    assert diameter_cells(d_cm, cells_per_cm3=cells_per_cm3) == pytest.approx(n_cells, rel=1e-3)
+
+
+@pytest.mark.parametrize('n_cells, cells_per_cm3, d_cm',
+                         [(0.0, 1e9, 0), (1e9, 1e9, 1.2407)])
+def test_cells_diameter(n_cells, cells_per_cm3, d_cm):
+
+    assert cells_diameter(n_cells, cells_per_cm3=cells_per_cm3) == pytest.approx(d_cm, rel=1e-3)
